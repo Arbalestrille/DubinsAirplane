e2176e1d67dc64b20ab9ab8b74f6e296b6ff0b0e
Arbalestrille <arnaud.lepers@polytechnique.edu>
2020-06-25 16:05:27 +0200

    MAINT: pep8 and parentheses
    apply autopep8 and add addition of parentheses :  print' ' -> print('')

--- python/ElementaryFunctions.py
+++ python/ElementaryFunctions.py
@@ -1,17 +1,17 @@
 #       __DUBINSAIRPLANEFUNCTIONS__
 #       A small set of helping functions
 #
-#       Authors: 
+#       Authors:
 #       Kostas Alexis (konstantinos.alexis@mavt.ethz.ch)
 
-import __builtin__
-    
+
 def max(a, b):
     # just a simple max function between 2 values
-    if a >=b:
+    if a >= b:
         return a
     else:
         return b
+
 
 def min(a, b=0, nargout=0):
     # just a simple min function between 2 values
